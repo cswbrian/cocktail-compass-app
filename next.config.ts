8b118407f0fcbdd8700942750b118e4b39b3b80e
Brian Leung <brianleung1017@gmail.com>
2025-02-12 02:27:03 +0800

    fix deploy script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/cocktail-compass-app",
+  assetPrefix: "/cocktail-compass-app/",
 };
 
 export default nextConfig;
