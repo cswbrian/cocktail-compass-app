e115cebf566f63dbbb7eaf6257e651164418f30f
Brian Leung <brianleung1017@gmail.com>
2025-02-14 02:10:52 +0800

    add custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/cocktail-compass-app",
-  assetPrefix: "/cocktail-compass-app/",
 };
 
 export default nextConfig;
