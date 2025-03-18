1ea9d631fda567a95c86fb4d9ad12c08c68ca493
Brian Leung <brianleung1017@gmail.com>
2025-03-19 01:27:32 +0800

    Updates

---
+++ _next/static/pg0vLrmdvajtev9fiLovj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[language]","\u002F[language]\u002Fbookmarks","\u002F[language]\u002Fcocktails\u002F[slug]","\u002F[language]\u002Fflavours\u002F[slug]","\u002F[language]\u002Fingredients\u002F[slug]","\u002F[language]\u002Ftwist"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
