a7104dbaeb2638ca10dfe5cea750106e20f5e3ae
Brian Leung <brianleung1017@gmail.com>
2025-04-02 01:49:10 +0800

    add swcMinify and header

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   compress: true,
+  swcMinify: true,
   images: {
     unoptimized: true,
     remotePatterns: [
