3c2e6abfa74620580ede856aeb6be3263a5cc414
Brian Leung <brianleung1017@gmail.com>
2025-05-14 01:18:46 +0800

    fix type

--- components/journal/feeds.tsx
+++ components/journal/feeds.tsx
@@ -1,6 +1,4 @@
 import { CocktailLog } from "@/types/cocktail-log";
-import { useLanguage } from "@/context/LanguageContext";
-import { translations } from "@/translations";
 import { CocktailLogList } from "@/components/cocktail-log/cocktail-log-list";
 
 interface FeedsProps {
@@ -12,8 +10,6 @@ interface FeedsProps {
 }
 
 export function Feeds({ logs, isLoading, onLogSaved, onLogDeleted }: FeedsProps) {
-  const { language } = useLanguage();
-  const t = translations[language];
 
   if (isLoading) {
     return null; // Loading state is now handled by AuthWrapper
